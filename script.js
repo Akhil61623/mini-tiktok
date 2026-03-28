229434e084ca4fb0a35c717f710b52cfa15472c8
Akhil61623 <akhilagrawal378@gmail.com>
2026-03-28 23:44:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ let index = 0;
 
 function nextVideo() {
   index = (index + 1) % videos.length;
+let startY = 0;
+
+document.addEventListener("touchstart", function(e) {
+  startY = e.touches[0].clientY;
+});
+
+document.addEventListener("touchend", function(e) {
+  let endY = e.changedTouches[0].clientY;
+
+  if (startY - endY > 50) {
+    nextVideo(); // swipe up
+  }
+
+  if (endY - startY > 50) {
+    nextVideo(); // swipe down (optional)
+  }
+});
   document.getElementById("videoPlayer").src = videos[index];
 }
