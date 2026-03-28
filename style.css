4ab2e484bde4ededb6e472d28c68e5568e4f50e9
Akhil61623 <akhilagrawal378@gmail.com>
2026-03-28 23:27:53 +0530

    Add video player functionality to style.css

---
+++ style.css
@@ -0,0 +1,12 @@
+let videos = [
+  "videos/video1.mp4",
+  "videos/video2.mp4",
+  "videos/video3.mp4"
+];
+
+let index = 0;
+
+function nextVideo() {
+  index = (index + 1) % videos.length;
+  document.getElementById("videoPlayer").src = videos[index];
+}
